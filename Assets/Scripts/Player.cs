80463613481f7157a7d3161c471cc581e17c51b2
Lennie Gutierrez <lenniegutierrez@getbellhops.com>
2020-12-02 23:09:29 -0500

    wip

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D _rigidbody2D;
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+    
+    void Update()
+    {
+        var horizontal = Input.GetAxis("Horizontal");
+        _rigidbody2D.velocity = new Vector2(horizontal, _rigidbody2D.velocity.y);
+    }
+}
